9a6950b3f99998d2a0e6880cd10a6ed1d8e251a5
SeriousMike <seriousmike88@gmail.com>
2014-08-08 00:54:43 +0400

    correct video replacement + video fragment

--- src/ru/seriousmike/hellomoon/VideoMoonFragment.java
+++ src/ru/seriousmike/hellomoon/VideoMoonFragment.java
@@ -6,31 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideoMoonFragment extends Fragment {
 	
-	private Button mPlayMovie;
 	private VideoView mVideoPlayer;
+	private MediaController mMediaController;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_video_moon, parent, false);
 		
-		mVideoPlayer = new VideoView( getActivity() );
+		mVideoPlayer = (VideoView) v.findViewById(R.id.videoView);
 		
-		mPlayMovie = (Button) v.findViewById(R.id.btnVideoPlay);
-		mPlayMovie.setOnClickListener( new View.OnClickListener() {
-			@Override 
-			public void onClick(View v) {
-				Uri uri = Uri.parse("android:resource://ru.seriousmike.hellomoon/raw/apollo_17_stroll");
-				mVideoPlayer.setVideoURI(uri);
-				mVideoPlayer.start();
-			}
-		} );
+		mMediaController = new MediaController( getActivity() );
+		mMediaController.setAnchorView( mVideoPlayer );
+		mVideoPlayer.setMediaController( mMediaController );
 		
+		Uri uri = Uri.parse("android.resource://ru.seriousmike.hellomoon/"+R.raw.videoviewdemo);
+		mVideoPlayer.setVideoURI(uri);
 		
+		mVideoPlayer.start();
 		
 		return v;
 	}
