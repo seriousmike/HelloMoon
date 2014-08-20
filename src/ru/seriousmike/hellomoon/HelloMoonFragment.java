2653506cf35c94c89904c25f0bc92641411ab690
SeriousMike <seriousmike88@gmail.com>
2014-08-20 23:21:19 +0400

    chapter 13 - saving fragment

--- src/ru/seriousmike/hellomoon/HelloMoonFragment.java
+++ src/ru/seriousmike/hellomoon/HelloMoonFragment.java
@@ -18,6 +18,12 @@ public class HelloMoonFragment extends Fragment {
 
 	
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+	
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_hello_moon,  parent, false);
 		
